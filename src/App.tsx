c41be6e0df931f73335718b9f5a3f1e3b02c2379
rinrin0401rinrin-sketch <rinrin0401rinrin@icloud.com>
2026-09-02 19:10:47 +0900

    fix: fit mobile tab changes in viewport

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
 import { NotFoundPage } from './routes/NotFoundPage';
 import { ElectionDetailPage } from './routes/ElectionDetailPage';
 import { GlossaryPage } from './routes/GlossaryPage';
@@ -7,21 +8,34 @@ import { MemberPage } from './routes/MemberPage';
 import { PrefecturePage } from './routes/PrefecturePage';
 import { SpecialFeaturePage } from './routes/SpecialFeaturePage';
 
+function ScrollToTopOnNavigation() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+  }, [location.key]);
+
+  return null;
+}
+
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/live" element={<SpecialFeaturePage pageId="live" />} />
-      <Route path="/map" element={<SpecialFeaturePage pageId="map" />} />
-      <Route path="/parties" element={<SpecialFeaturePage pageId="parties" />} />
-      <Route path="/proportional" element={<SpecialFeaturePage pageId="proportional" />} />
-      <Route path="/archive" element={<SpecialFeaturePage pageId="archive" />} />
-      <Route path="/glossary" element={<GlossaryPage />} />
-      <Route path="/elections/:electionId" element={<ElectionDetailPage />} />
-      <Route path="/elections/:electionId/prefectures/:prefectureId" element={<PrefecturePage />} />
-      <Route path="/elections/:electionId/members/:memberId" element={<MemberPage />} />
-      <Route path="/404" element={<NotFoundPage />} />
-      <Route path="*" element={<Navigate to="/404" replace />} />
-    </Routes>
+    <>
+      <ScrollToTopOnNavigation />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/live" element={<SpecialFeaturePage pageId="live" />} />
+        <Route path="/map" element={<SpecialFeaturePage pageId="map" />} />
+        <Route path="/parties" element={<SpecialFeaturePage pageId="parties" />} />
+        <Route path="/proportional" element={<SpecialFeaturePage pageId="proportional" />} />
+        <Route path="/archive" element={<SpecialFeaturePage pageId="archive" />} />
+        <Route path="/glossary" element={<GlossaryPage />} />
+        <Route path="/elections/:electionId" element={<ElectionDetailPage />} />
+        <Route path="/elections/:electionId/prefectures/:prefectureId" element={<PrefecturePage />} />
+        <Route path="/elections/:electionId/members/:memberId" element={<MemberPage />} />
+        <Route path="/404" element={<NotFoundPage />} />
+        <Route path="*" element={<Navigate to="/404" replace />} />
+      </Routes>
+    </>
   );
 }
